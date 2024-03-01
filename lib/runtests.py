16cfaef0a6371b08f5a3dbb326ca1392646c36f8
GabrielFRails <gabriel.estudy.reis@gmail.com>
2024-03-01 10:37:16 -0300

    Add runtests.py to use custom lib and class to run tests

---
+++ lib/runtests.py
@@ -0,0 +1,10 @@
+# 
+# Copyright (c) Gabriel Freitas: gabriel.estudy.reis@gmail.com
+#
+
+import libunittest as ut
+
+from testcase_pokeapi import *
+
+ut.add_testcase_list(testcaselist_pokeapi)
+ut.run()
