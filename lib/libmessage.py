8810052448185466ead9180a21f7fd62af01e83d
GabrielFRails <gabriel.estudy.reis@gmail.com>
2024-03-06 14:59:44 -0300

    Create libmessage, a wrapper for rabbitmq and pika

---
+++ lib/libmessage.py
@@ -0,0 +1,31 @@
+# 
+# Copyright (c) Gabriel Freitas: gabriel.estudy.reis@gmail.com
+# Wrapper for rabbitmq and pika
+#
+
+import pika
+
+def send_message(message: str, queue_name="default"):
+# {
+	connection = get_pika_conection()
+	channel = connection.channel()
+	channel.queue_declare(queue=queue_name)
+	channel.basic_publish(exchange='', routing_key=queue_name, body=message)
+# }
+
+__pika_connection = None
+def get_pika_conection():
+# {
+	global __pika_connection
+	if not __pika_connection:
+		__pika_connection =  pika.BlockingConnection(
+			pika.ConnectionParameters(host='localhost'))
+
+	return __pika_connection
+# }
+
+def close_connection():
+# {
+	connection = get_pika_conection()
+	connection.close()
+# }
