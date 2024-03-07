ba0ba98f0bf81dd66d4cc7139d55ab794921505d
GabrielFRails <gabriel.estudy.reis@gmail.com>
2024-03-07 09:39:05 -0300

    Use tabs for identation instead of 4 spaces

--- doc/rabbitmq/receive_example.py
+++ doc/rabbitmq/receive_example.py
@@ -2,25 +2,25 @@
 import pika, sys, os
 
 def main():
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
-    channel = connection.channel()
+	connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+	channel = connection.channel()
 
-    channel.queue_declare(queue='hello')
+	channel.queue_declare(queue='hello')
 
-    def callback(ch, method, properties, body):
-        print(f" [x] Received {body}")
+	def callback(ch, method, properties, body):
+		print(f" [x] Received {body}")
 
-    channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)
+	channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)
 
-    print(' [*] Waiting for messages. To exit press CTRL+C')
-    channel.start_consuming()
+	print(' [*] Waiting for messages. To exit press CTRL+C')
+	channel.start_consuming()
 
 if __name__ == '__main__':
-    try:
-        main()
-    except KeyboardInterrupt:
-        print('Interrupted')
-        try:
-            sys.exit(0)
-        except SystemExit:
-            os._exit(0)
+	try:
+		main()
+	except KeyboardInterrupt:
+		print('Interrupted')
+		try:
+			sys.exit(0)
+		except SystemExit:
+			os._exit(0)
