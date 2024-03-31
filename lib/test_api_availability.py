1325254fc56e3523bf675dd247ee6110d4d60cc2
GabrielFRails <gabriel.estudy.reis@gmail.com>
2024-03-31 20:27:32 -0300

    Create evolution group availability testcase

--- lib/test_api_availability.py
+++ lib/test_api_availability.py
@@ -77,9 +77,27 @@ class EncounterGroupAvailabilityTestCase(unittest.TestCase):
 			r = request_get(url)
 			self.assertEqual(r.status_code, 200)
 
+class EvolutionGroupAvailabilityTestCase(unittest.TestCase):
+	def setUp(self):
+		return True
+
+	def tearDown(self):
+		return True
+
+	def test_evolution_group_availability(self):
+		pokemon_group_endpoint_list = [
+			"evolution-chain", "evolution-trigger"
+		]
+
+		for endpoint in pokemon_group_endpoint_list:
+			url = f"https://pokeapi.co/api/v2/{endpoint}"
+			r = request_get(url)
+			self.assertEqual(r.status_code, 200)
+
 testcaselist_availability = [
 	PokemonGroupAvailabilityTestCase,
 	BerriesGroupAvailabilityTestCase,
 	ContestGroupAvailabilityTestCase,
-	EncounterGroupAvailabilityTestCase
+	EncounterGroupAvailabilityTestCase,
+	EvolutionGroupAvailabilityTestCase
 ]
