c8db9744f1f9d8c82575c2ee91893f3014b035dc
GabrielFRails <gabriel.estudy.reis@gmail.com>
2024-05-27 19:08:46 -0300

    Update libmessage

--- lib/libmessage.py
+++ lib/libmessage.py
@@ -7,6 +7,10 @@ import pika
 import json
 import time
 import datetime
+import os
+
+rabbitmq_host = os.getenv('RABBITMQ_HOST', 'localhost')
+rabbitmq_port = int(os.getenv('RABBITMQ_PORT', '5672'))
 
 def send_message(message: str, queue_name: str, channel):
 # {
@@ -28,6 +32,20 @@ def generate_unittest_message(testid: str, test_status: str) -> str:
 	return msg_json
 # }
 
+def generate_command_message(command: str, origin: str) -> str:
+# {
+	ts = time.time()
+	message = {
+		"origin": origin,
+		"command": command,
+		"ts": ts,
+		"ts_str": convert_ts_to_date(ts)
+	}
+
+	msg_json = json.dumps(message)
+	return msg_json
+# }
+
 def convert_ts_to_date(timestamp):
     # Convert timestamp to datetime object
     dt_object = datetime.datetime.fromtimestamp(timestamp)
@@ -38,19 +56,30 @@ def convert_ts_to_date(timestamp):
     return formatted_time
 
 __pika_connection = None
-def get_pika_conection():
+def get_pika_connection():
 # {
 	global __pika_connection
 	if not __pika_connection:
 		__pika_connection =  pika.BlockingConnection(
-			pika.ConnectionParameters(host='localhost'))
+			pika.ConnectionParameters(host=rabbitmq_host, port=rabbitmq_port))
 
 	return __pika_connection
 # }
 
+__unittest_msg_channel = None
+def get_msg_channel():
+# {
+	global __unittest_msg_channel
+	if not __unittest_msg_channel:
+		connection = get_pika_connection()
+		__unittest_msg_channel = message_open_channel(connection)
+
+	return __unittest_msg_channel
+# }
+
 def close_connection():
 # {
-	connection = get_pika_conection()
+	connection = get_pika_connection()
 	connection.close()
 # }
 
