6d79510b6a568e4462051b119525cfc4cd1cadeb
GabrielFRails <gabriel.estudy.reis@gmail.com>
2024-05-28 19:40:36 -0300

    Create liblog

---
+++ lib/liblog.py
@@ -0,0 +1,19 @@
+import logging
+from datetime import datetime
+
+def log_info(s: str):
+# {
+	logger = _logger()
+	logger.info(f" {datetime.now()}: {s}")
+# }
+
+__logger = None
+def _logger():
+# {
+	global __logger
+	if not __logger:
+		logging.basicConfig(level=logging.INFO)
+		__logger = logging.getLogger(__name__)
+	
+	return __logger
+# }
